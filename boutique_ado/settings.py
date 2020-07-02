7d81a19df8c00ec0bd727cde8648c16b7c9306be
Christopher Riley <chris_riley1993@hotmail.com>
2020-07-02 20:44:51 +0000

    046 profiles part 6 - completed confirmation emails

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -126,7 +126,6 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -173,3 +172,4 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
