28ce337dd15b77390e79fca44ac498775492a211
Christopher Riley <chris_riley1993@hotmail.com>
2020-07-03 16:12:35 +0000

    Product admin - Product form

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -17,7 +17,6 @@ def webhook(request):
     print(wh_secret)
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
-
     # Get the webhook data and verify its signature
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
